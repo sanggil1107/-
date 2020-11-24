668e7a1a496b9e137732a2357cd1e57fe8417e2d
sanggil1107 <yangpang1107@gmail.com>
2020-11-25 00:01:35 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import MyComponent from './component/chap3/MyComponent';
+import Say from './component/chap3/Say';
 
 const App = () => {
-  return <MyComponent name="react" num={3}>리액트</MyComponent>;
-}
+  return (
+    <div>
+      <MyComponent name="react" num={3}>리액트</MyComponent>
+      <Say/>
+    </div>
+  );
+};
 
 export default App;
