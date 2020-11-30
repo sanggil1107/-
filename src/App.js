0e18d24eef8b69e442c64c72d05babea9c2afd52
sanggil1107 <yangpang1107@gmail.com>
2020-11-30 23:47:38 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import Average from './component/chap8/Average';
 import Counter from './component/chap8/Counter';
 import Info from './component/chap8/Info';
+import StyledComponents from './component/chap9/StyledComponents';
 
 const App = () => {
   return (
-    <Average/>
+    <StyledComponents/>
   );
 };
 
