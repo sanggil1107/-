314995595c59aa59f539e4f6b52875dbdcc18596
sanggil1107 <yangpang1107@gmail.com>
2020-12-02 00:26:20 +0900

    Todo Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import TodoInsert from './component/chap10/TodoInsert';
 import TodoTemplate from './component/chap10/TodoTemplate';
+import TodoList from './component/chap10/TodoList';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert/>
+      <TodoList/>
     </TodoTemplate>
   );
 };
