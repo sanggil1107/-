f6bf63e782e78666b99668bcb8b9e19a54cc100c
sanggil1107 <yangpang1107@gmail.com>
2020-12-15 18:12:14 +0900

    context api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useCallback, useState } from 'react';
-import NewsPage from './component/chap12/NewsPage';
-import { Route } from 'react-router-dom';
+// import React, { useCallback, useState } from 'react';
+// import NewsPage from './component/chap12/NewsPage';
+// import { Route } from 'react-router-dom';
+
+// const App = () => {
+ 
+//   return (
+//     <Route path="/:category?" component={NewsPage} />
+//   );
+// };
+
+// export default App;
+
+import React from 'react';
+import ColorBox from './component/chap13/ColorBox';
+import { ColorProvider } from './component/chap13/Color';
+import SelectColors from './component/chap13/SelectColors';
 
 const App = () => {
- 
   return (
-    <Route path="/:category?" component={NewsPage} />
+    <ColorProvider>
+      <div>
+        <SelectColors/>
+        <ColorBox/>
+      </div>
+    </ColorProvider>
   );
 };
 
---
+++ src/component/chap13/Color.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+
+const ColorContext = createContext({
+  state: { color: 'black', subcolor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {}
+  }
+});
+
+const ColorProvider = ({children}) => {
+  const [color, setColor] = useState('black');
+  const [subcolor, setSubcolor] = useState('red');
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor }
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer
+const { Consumer: ColorConsumer } = ColorContext;
+
+export { ColorConsumer, ColorProvider };
+
+export default ColorContext;
